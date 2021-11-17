b41bca690ad7d027f9a680ac290b769ba2c8ec35
kyosuke ito <kyosuke.ito@gmail.com>
2021-11-16 22:37:58 -0800

    update fileuploadpage and change webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,46 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
+  mode: "development",
+  entry: "./src/app.js",
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
   },
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
-    maxAssetSize: 512000
+    maxAssetSize: 512000,
   },
   module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
+    rules: [
+      {
+        use: ["babel-loader"],
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
-  devtool: 'cheap-source-map',
+  devtool: "cheap-source-map",
   devServer: {
-    static:
-    {
-      directory: path.join(__dirname, 'public'),
+    static: {
+      directory: path.join(__dirname, "public"),
     },
-      historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
 };
